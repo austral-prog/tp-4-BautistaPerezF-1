209bb3b1271d9900cdc002602428ed4c59228d0f
BautistaPerezF <162722097+BautistaPerezF@users.noreply.github.com>
2025-04-21 11:27:09 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,7 @@
 def leap_year():
-    print("TO DO")
+    
+     año = int(input("Ingrese un año: "))
+     if (año % 4 == 0 and año % 100 != 0)  or (año % 400 == 0):
+         return (f"El año {año} es biciesto")
+     else:
+         return (f"El año {año} no es biciesto")
